3514439c8ee09530536a0f4e2e6d5964d99dde06
Baha Rojbi <101024667+Baha-Rojbi@users.noreply.github.com>
2023-10-01 23:18:38 +0100

    Programming with JavaScript -W3
    week 3 FP

---
+++ Learning.js
@@ -0,0 +1,11 @@
+function Icecream(flavor) {
+    this.flavor = flavor;
+    this.meltIt = function() {
+        console.log(`The ${this.flavor} icecream has melted`);
+    }
+}
+
+let kiwiIcecream = new Icecream("kiwi");
+let appleIcecream = new Icecream("apple");
+console.log(kiwiIcecream);
+console.log(appleIcecream);
