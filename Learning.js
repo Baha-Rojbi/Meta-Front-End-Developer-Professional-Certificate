e698e39e4d4a22149a5716406551549973f89bda
Baha Rojbi <101024667+Baha-Rojbi@users.noreply.github.com>
2023-10-10 23:09:10 +0100

    programming with JS W3
    ALMOST DONE WITH WEEK 3

--- Learning.js
+++ Learning.js
@@ -1,83 +1,135 @@
-class Train {
-    constructor(color, lightsOn) {
+class Animal {
+    constructor(color = 'yellow', energy = 100) {
         this.color = color;
-        this.lightsOn = lightsOn;
+        this.energy = energy;
     }
-    toggleLights() {
-        this.lightsOn = !this.lightsOn;
+    isActive() {
+        if(this.energy > 0) {
+            this.energy -= 20;
+            console.log('Energy is decreasing, currently at:', this.energy)
+        } else if(this.energy == 0){
+            this.sleep();
+        }
     }
-    lightsStatus() {
-        console.log('Lights on?', this.lightsOn);
+    sleep() {
+        this.energy += 20;
+        console.log('Energy is increasing, currently at:', this.energy)
     }
-    getSelf() {
-        console.log(this);
-    }
-    getPrototype() {
-        var proto = Object.getPrototypeOf(this);
-        console.log(proto);
-    }
-}
-
-class HighSpeedTrain extends Train {
-    constructor(passengers, highSpeedOn, color, lightsOn) {
-        super(color, lightsOn);
-        this.passengers = passengers;
-        this.highSpeedOn = highSpeedOn;
-    }
-    toggleHighSpeed() {
-        this.highSpeedOn = !this.highSpeedOn;
-        console.log('High speed status:', this.highSpeedOn);
-    }
-    toggleLights() {
-        super.toggleLights();
-        super.lightsStatus();
-        console.log('Lights are 100% operational.');
+    getColor() {
+        console.log(this.color)
     }
 }
 
-var myFirstTrain = new Train('red', false);
-console.log(myFirstTrain); // Train {color: 'red', lightsOn: false}
-var mySecondTrain = new Train('blue', false);
-var myThirdTrain = new Train('blue', false);
-
-var train4 = new Train('red', false);
-train4.toggleLights(); // undefined
-train4.lightsStatus(); // Lights on? true
-train4.getSelf(); // Train {color: 'red', lightsOn: true}
-train4.getPrototype(); // {constructor: f, toggleLights: f, ligthsStatus: f, getSelf: f, getPrototype: f}
-
-var train5 = new Train('blue', false);
-var highSpeed1 = new HighSpeedTrain(200, false, 'green', false);
-
-train5.toggleLights(); // undefined
-train5.lightsStatus(); // Lights on? true
-highSpeed1.toggleLights(); // Lights on? true, Lights are 100% operational.
-
-//////////////////
-class StationaryBike {
-    constructor(position, gears) {
-        this.position = position
-        this.gears = gears
+class Cat extends Animal {
+    constructor(sound = 'purr', canJumpHigh = true, canClimbTrees = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canClimbTrees = canClimbTrees;
+        this.canJumpHigh = canJumpHigh;
+    }
+    makeSound() {
+        console.log(this.sound);
     }
 }
 
-class Treadmill {
-    constructor(position, modes) {
-        this.position = position
-        this.modes = modes
+class Bird extends Animal {
+    constructor(sound = 'chirp', canFly = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canFly = canFly;
+    }
+    makeSound() {
+        console.log(this.sound);
     }
 }
 
-class Gym {
-    constructor(openHrs, stationaryBikePos, treadmillPos) {
-        this.openHrs = openHrs
-        this.stationaryBike = new StationaryBike(stationaryBikePos, 8)
-        this.treadmill = new Treadmill(treadmillPos, 5)
+class HouseCat extends Cat {
+    constructor(houseCatSound = "meow", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.houseCatSound = houseCatSound;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        console.log(this.houseCatSound);
     }
 }
 
-var boxingGym = new Gym("7-22", "right corner", "left corner")
+class Tiger extends Cat {
+    constructor(tigerSound = "Roar!", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.tigerSound = tigerSound;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        console.log(this.tigerSound);
+    }
+}
 
-console.log(boxingGym.openHrs) //
-console.log(boxingGym.stationaryBike) //
-console.log(boxingGym.treadmill) //
+class Parrot extends Bird {
+    constructor(canTalk = false, sound,canFly, color,energy) {
+        super(sound,canFly, color,energy);
+        this.canTalk = canTalk;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        if (this.canTalk) {
+            console.log("I'm a talking parrot!");
+        }
+    }
+}
+
+var fiji = new Parrot(false); // we're passing `false` to the constructor so that fiji can't talk
+var polly = new Parrot(true); // we're passing `true` to the constructor so that polly can talk
+
+fiji.makeSound(); // undefined
+fiji.makeSound(true); // chirp
+
+polly.makeSound(); // I'm a talking parrot!
+polly.makeSound(true); // chirp, I'm a talking parrot!
+
+polly.color; // yellow
+polly.energy; // 100
+
+polly.isActive(); // Energy is decreasing, currently at: 80
+
+var penguin = new Bird("shriek", false, "black and white", 200); // setting all the custom properties
+penguin; // Bird {color: 'black and white', energy: 200, sound: 'shriek', canFly: false }
+
+penguin.sound; // 'shriek'
+penguin.canFly; // false
+penguin.color; // 'black and white'
+penguin.energy; // 200
+penguin.isActive(); // Energy is decreasing, currently at: 180
+
+var leo = new HouseCat();
+
+// leo, no purring please:
+leo.makeSound(false); // meow
+// leo, both purr and meow now:
+leo.makeSound(true); // purr, meow
+
+var cuddles = new Tiger();
+cuddles.makeSound(false); // Roar!
+cuddles.makeSound(true); // purr, Roar!
+
+////////////////////
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( const key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
+}
+///
+const meal=["soup", "steak", "ice cream"];
+let [starter]=meal;
+console.log(starter);
--- OOP/ooprogramming.js
+++ OOP/ooprogramming.js
@@ -1,48 +1,42 @@
 // Task 1: Code a Person class
-class Person {
-    constructor(name = "Tom", age = 20, energy = 100) {
-      this.name = name;
-      this.age = age;
-      this.energy = energy;
+class Person{
+    constructor(name = "Tom", age = 20, energy = 100){
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
     }
-  
-    sleep() {
-      this.energy += 10;
+    sleep(){
+        this.energy += 10;
     }
-  
-    doSomethingFun() {
-      this.energy -= 10;
+    doSomethingFun(){
+        this.energy -= 10;
     }
-  }
-  
-  // Task 2: Code a Worker class
-  class Worker extends Person {
-    constructor(name, age, energy, xp = 0, hourlyWage = 10) {
-      super(name, age, energy);
-      this.xp = xp;
-      this.hourlyWage = hourlyWage;
+}
+// Task 2: Code a Worker class
+
+class Worker extends Person {
+    constructor(name, age, energy, xp= 0, hourlyWage = 10){
+        super(name, age, energy);
+        this.xp = xp;	
+        this.hourlyWage = hourlyWage;
     }
-  
-    goToWork() {
-      this.xp += 10;
+    goToWork(){
+        this.xp += 10;
     }
-  }
-  
-  // Task 3: Code an intern object
-  function intern() {
-    const internObject = new Worker("Bob", 21, 110, 0, 10);
-    internObject.goToWork();
-    return internObject;
-  }
-  
-  // Task 4: Code a manager object
-  function manager() {
-    const managerObject = new Worker("Alice", 30, 120, 100, 30);
-    managerObject.doSomethingFun();
-    return managerObject;
-  }
-  
-  // Testing
-  console.log("Intern Object:", intern());
-  console.log("Manager Object:", manager());
-  
+}
+
+// Task 3: Code an intern object, run methods
+function intern() {
+    let newIntern = new Worker("Bob", 21, 110, 0, 10);
+    newIntern.goToWork();
+    return newIntern;
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+    let newManager = new Worker("Alice", 30, 120, 100, 30);
+    newManager.doSomethingFun();
+    return newManager;
+}
+console.log("Intern Object:", intern());
+console.log("Manager Object:", manager());
